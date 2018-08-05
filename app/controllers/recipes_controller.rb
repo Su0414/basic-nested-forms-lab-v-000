2279c8e7dadc1648ece5d1545872993a133f3ce2
Suchitra Naik <susanaik@gmail.com>
2018-08-05 14:14:10 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,5 +13,14 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:id, :name, :quantity])
+  end
+
 end
